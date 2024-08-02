4e62e3cc91f71e0e49e81cfd9dfd903b9585262f
paunchymonk <gregory@dickison.net>
2024-08-02 09:19:15 -0700

    Styling tweaks

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,17 +6,23 @@ function Hero() {
     <div className="relative bg-gray-50 w-full">
       <main className="lg:relative">
         <div className="mx-auto w-full pb-20 pt-16 text-center xlg:py-48 xl:text-left">
-          <div className="px-6 sm:px-8 xl:w-2/3 xl:pr-16 flex flex-col gap-6 items-center xl:items-start">
-            <h1 className="text-4xl font-normal tracking-wide text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Imago Dei Insurance Advisors</span>
-            </h1>
-            <p className="mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Bringing a <span className="underline">Christian Worldview</span> to Health Benefits</p>
+          <div className="px-6 sm:px- xl:w-2/3 xl:pr-16 flex flex-col gap-6 items-center xl:items-start">
+            <div className="my-0 text-4xl font-normal tracking-wide text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+              <h1>
+                <span className="block xl:inline">Imago Dei</span>
+              </h1>
+              <h1>
+                <span className="block xl:inline">Insurance Advisors</span>
+              </h1>
+
+            </div>
+            <p className="mt-3 max-w-md text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">Bringing a <span className="underline italic font-semibold"> Christian Worldview </span> to Health Benefits</p>
             <div className="flex flex-col gap-2">
               <p className="mt-3 max-w-md text-md text-gray-500 sm:text-lg md:max-w-5xl">As a business owner, you are called to <span className="underline">love your people</span> and <span className="underline">steward your assets</span>.</p>
               <p className="mt-3 max-w-md text-md text-gray-500 sm:text-lg md:max-w-5xl">We can help you save <span className="italic underline">20%-40%</span> on your employee health care plan.</p>
             </div>
             <div className="mt-10 sm:flex sm:justify-center xl:justify-start">
-              <div className="rounded-md shadow">
+              <div className="rounded-full shadow">
                 <a href="#" className="flex w-full items-center justify-center rounded-full border border-transparent bg-blue-600 px-8 py-3 text-base font-medium text-white hover:bg-blue-700 md:px-10 md:py-4 md:text-lg">Get a Free Consultation</a>
               </div>
             </div>
