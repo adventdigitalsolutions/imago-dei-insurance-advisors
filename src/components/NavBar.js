4f85f706d9f2aa660e29a4cf2ed3751e1549c03f
paunchymonk <gregory@dickison.net>
2024-09-16 19:19:18 -0700

    Add favicon, remove nav hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,9 +27,9 @@ export default function NavBar () {
               </div>
             </div>
           </div>
-          <div className="md:hidden">
+          {/* <div className="md:hidden">
             <MenuBurger/>
-          </div>
+          </div> */}
         </div>
       </div>
     </header>
