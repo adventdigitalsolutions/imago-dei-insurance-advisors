61e870cbcf57de2e32818973a8e347bd810d5861
Bruce Johnson <johnswanson3.9@gmail.com>
2025-06-23 12:52:30 -0700

    updates top page header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,22 @@ export default function Home() {
           alt="People Chatting"
           width={800}
           height={400}
-          className="w-full h-auto rounded mb-4 max-h-4xl"
+          className="w-full h-auto rounded mb-4 max-h-[700px] object-cover"
         />
-        <div className="absolute inset-0 rounded bg-royal-purple/50 pointer-events-none" />
+        <div className="absolute inset-0 rounded bg-royal-purple/70 pointer-events-none pl-32 text-white">
+          <div className="mt-20 lg:mt-52">
+            <div className="text-[100px] font-montserrat font-bold">
+              Imago Dei
+            </div>
+            <div className="font-extralight text-[60px] mt-[-20px]">
+              Insurance Advisors
+            </div>
+          </div>
+          <div className="mt-10 text-3xl max-w-md font-medium">
+            <div className="underline inline">Christian values</div> in a
+            benefits plan you can trust
+          </div>
+        </div>
       </div>
       <div className="max-w-4xl mx-auto">
         <div className="bg-white rounded-lg shadow-lg p-6 mb-4">
