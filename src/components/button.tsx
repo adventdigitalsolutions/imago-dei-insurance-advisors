cdcb8ddd21cca261c231c84abc960c13e886401a
Bruce Johnson <johnswanson3.9@gmail.com>
2025-06-27 19:17:13 -0700

    creates hero

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,14 +11,22 @@ export const Button = ({
   children,
   className = '',
   size = 'default',
+  variant = 'default',
   ...props
 }: ButtonProps) => {
   const baseClasses =
-    'bg-royal-purple w-fit text-white rounded-[30px] cursor-pointer text-base hover:bg-gray-300 transition-all duration-300 hover:shadow-lg shadow-none';
-  const sizeClasses = size === 'small' ? 'px-3 py-4 text-sm' : 'px-4 py-4';
+    'bg-royal-purple w-fit text-white rounded-[30px] cursor-pointer text-lg hover:bg-gray-300 transition-all duration-300 hover:shadow-lg shadow-none';
+  const sizeClasses = size === 'small' ? 'px-4 py-3 text-sm' : 'px-6 py-3';
+  const deepBlueClasses =
+    variant === 'deepBlue'
+      ? 'bg-[#2B4C9D] hover:bg-[#2B4C9D]/80 border border-white border-2 border-solid hover:shadow-lg shadow-none'
+      : '';
 
   return (
-    <button className={`${baseClasses} ${sizeClasses} ${className}`} {...props}>
+    <button
+      className={`${baseClasses} ${sizeClasses} ${deepBlueClasses} ${className}`}
+      {...props}
+    >
       {children}
     </button>
   );
---
+++ src/components/page-components/homepage/hero.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { getCopy } from '@/getCopy';
+import { Button } from '@/components/button';
+
+export const Hero = () => (
+  <div className="min-h-screen w-full mt-20 relative overflow-clip">
+    <div className="relative">
+      <Image
+        src="/people-chatting.png"
+        alt="People Chatting"
+        width={800}
+        height={400}
+        className="w-full h-[480px] z-0 lg:h-auto mb-4 max-h-[700px] object-cover"
+      />
+      <div className="absolute inset-0 rounded bg-royal-purple/70 pl-9 lg:pl-20 text-white z-10 justify-center flex">
+        <div className="max-w-6xl w-full">
+          <div className="mt-16 lg:mt-32">
+            <div className="text-[44px] lg:text-[100px] font-montserrat font-bold">
+              {getCopy('hero.title')}
+            </div>
+            <div className="font-extralight text-[32px] lg:text-[60px] mt-[-20px]">
+              {getCopy('hero.subtitle')}
+            </div>
+          </div>
+          <div className="mt-10 text-2xl lg:text-3xl max-w-md font-medium pr-6 sm:pr-0">
+            <div className="underline inline">
+              {getCopy('hero.description1')}
+            </div>
+            {getCopy('hero.description2')}
+          </div>
+          <div className="mt-10 gap-4 lg:gap-0 w-72 lg:w-auto flex flex-col lg:flex-row">
+            <Button variant="deepBlue">{getCopy('hero.button1')}</Button>
+            <Button variant="deepBlue" className="lg:ml-4">
+              {getCopy('hero.button2')}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
