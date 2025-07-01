12ac954e1732ce42443aa1ed1f0611e7d771cfa8
Bruce Johnson <johnswanson3.9@gmail.com>
2025-06-30 19:21:50 -0700

    adds footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Montserrat, Lora } from 'next/font/google';
 import './globals.css';
 import { Navbar } from '@/components/navbar';
+import { Footer } from '@/components/footer';
 
 const montserrat = Montserrat({
   variable: '--font-montserrat',
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
