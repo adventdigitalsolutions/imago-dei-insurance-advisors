686dcd5c4cc8e1294e4ec135bc37aeaa0d54575d
Bruce Johnson <johnswanson3.9@gmail.com>
2025-07-02 14:54:00 -0700

    moblifies business-solutions-section

--- src/components/page-components/homepage/business-solutions-section.tsx
+++ src/components/page-components/homepage/business-solutions-section.tsx
@@ -4,20 +4,20 @@ import { RightArrow } from '@/components/icons/right-arrow';
 
 export const BusinessSolutionsSection = () => (
   <div
-    className="relative w-full h-full bg-cover bg-center bg-no-repeat py-40 overflow-clip"
+    className="relative w-full h-full bg-cover bg-center bg-no-repeat py-20 sm:py-40 overflow-clip"
     style={{
       backgroundImage: 'url(/grid_bg.png)',
     }}
   >
-    <div className="flex w-full gap-10">
-      <div className="py-28 w-2/3 bg-gray-100 text-black text-4xl rounded-l-none rounded-r-[80px] whitespace-pre-line font-light px-48 leading-relaxed tracking-[3px] content-center">
+    <div className="flex flex-col sm:flex-row w-full gap-10">
+      <div className="py-14 sm:py-28 w-11/12 sm:w-2/3 bg-gray-100 text-black text-xl sm:text-4xl rounded-l-none rounded-r-[80px] sm:whitespace-pre-line font-light px-10 sm:px-48 leading-relaxed tracking-[3px] content-center">
         {getCopy('businessSolutionsSection.box1Pt1')}
         <span className="underline decoration-gray-400 decoration-2">
           {getCopy('businessSolutionsSection.box1Pt2')}
         </span>
         {getCopy('businessSolutionsSection.box1Pt3')}
       </div>
-      <div className="py-28 px-16 w-1/3 whitespace-pre-line bg-gray-100 text-black text-4xl rounded-l-[80px] rounded-r-none">
+      <div className="py-10 pl-14 sm:py-28 sm:px-16 w-11/12 sm:w-1/3 whitespace-pre-line bg-gray-100 text-black text-3xl sm:text-4xl rounded-l-[80px] rounded-r-none overflow-clip self-end">
         {getCopy('businessSolutionsSection.box2')}
         <Button className="mt-6 flex items-center gap-2">
           <div className="flex items-center gap-2 pt-[2px]">
