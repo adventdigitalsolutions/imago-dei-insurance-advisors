4894109b7f222df382758b25092100a031fa0f9b
Bruce Johnson <johnswanson3.9@gmail.com>
2025-08-01 16:32:54 -0700

    adds sitemap autogen

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://imagodeinsuranceadvisors.com',
+  generateRobotsTxt: true, // Automatically creates robots.txt too
+};
